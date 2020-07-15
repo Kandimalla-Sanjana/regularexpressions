dcf3a3cfc720d7f76598ffd876e1b268970b8168
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-15 13:32:26 +0530

    First name validation

--- userregistration.sh
+++ userregistration.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
 echo "Welcome user!"
+
+function firstname()
+{
+	read -p "Enter the first name:" firstName
+	patternFirstName="^([[:upper:]]){1}[[:lower:]]{2,}$"
+	if [[ $firstName =~ $patternFirstName ]]
+	then
+        	echo "First name is valid"
+	else
+        	echo "First name is invalid"
+	fi
+}
+
+firstname
